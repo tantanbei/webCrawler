a66095ee75115a242e53c9f52fb6115490d077fe
tankaide <tkd@lbxtech.com>
2016-04-27 22:54:30 +0800

    ADD get chexiang car name api, save into db

--- main.go
+++ main.go
@@ -85,6 +85,29 @@ func updateMysql(db *sql.DB, id int, price string, remark string) {
 	}
 }
 
+//create the all name of cars on chexiang, and support chinese
+func CreatNameTable(db *sql.DB) {
+	_, err := db.Exec(fmt.Sprint("create table if not exists chexiangCarName(id int primary KEY not null, name varchar(100) not null)ENGINE=InnoDB DEFAULT CHARSET=utf8;"))
+	if err != nil {
+		fmt.Println("chexiangCarName Create", err)
+	}
+}
+
+//add the chexiang car name into table
+func updateNameTable(db *sql.DB, id int, name string) {
+	stmt, err := db.Prepare(fmt.Sprint("INSERT chexiangCarName SET id=?, name=?"))
+	defer stmt.Close()
+	if err != nil {
+		fmt.Println("updateMysql prepare", err)
+	}
+
+	_, err = stmt.Exec(id, name)
+	if err != nil {
+		fmt.Println("updateMysql execute", err)
+	}
+}
+
+//return the car name and price
 func ParseWebBody(body []byte) (string, string, error) {
 	reg, err := regexp.Compile("car-details.*\n.*<h1>(.*)</h1>")
 	if err != nil {
